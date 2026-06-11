11ee811a90ac0add45b02adbf9b0d89f89b6b9bb
nomisceyes <157041379+nomisceyes@users.noreply.github.com>
2026-06-11 19:11:24 +0300

    Update

--- Assets/_project/Scripts/GridSelector.cs
+++ Assets/_project/Scripts/GridSelector.cs
@@ -4,38 +4,43 @@ using UnityEngine.Tilemaps;
 public class GridSelector : MonoBehaviour
 {
     [SerializeField] private Tilemap _groundTilemap;
-    [SerializeField] private Tilemap _highlightTilemap;
-    [SerializeField] private TileBase _highlightPrefab;
 
     private Camera _camera;
-    private Vector3Int _currentHoverCell;
-    
+    private Color _highlightColor = new(1f, 1f, 0.5f, 0.7f);
+    private Color _originalColor;
+    private TileBase _lastHighlightTile;
+    private Vector3Int _currentHighlightCell;
+
     private void Start()
     {
         _camera = Camera.main;
+        _originalColor = Color.white;
+        _currentHighlightCell = new Vector3Int(-999, -999, -999);
     }
 
     private void Update()
     {
-        Vector3 mouseWorldPosition =  _camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mouseWorldPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPosition.z = 0;
-        
-        Vector3Int cellPosition =  _groundTilemap.WorldToCell(mouseWorldPosition);
 
-        if (_groundTilemap.HasTile(cellPosition))
+        Vector3Int cellPosition = _groundTilemap.WorldToCell(mouseWorldPosition);
+
+        if (_currentHighlightCell != cellPosition)
         {
-            if (_currentHoverCell != cellPosition)
+            if (_groundTilemap.HasTile(_currentHighlightCell))
             {
-                _currentHoverCell = cellPosition;
-                
-                UpdateHightlight(_currentHoverCell);
+                _groundTilemap.SetColor(_currentHighlightCell, _originalColor);
+            }
+
+            if (_groundTilemap.HasTile(cellPosition))
+            {
+                _groundTilemap.SetColor(cellPosition, _highlightColor);
+                _currentHighlightCell = cellPosition;
+            }
+            else
+            {
+                _currentHighlightCell = new Vector3Int(-999, -999, -999);
             }
         }
-    }
-
-    private void UpdateHightlight(Vector3Int cellPosition)
-    {
-        _highlightTilemap.ClearAllTiles();
-        _highlightTilemap.SetTile(cellPosition, _highlightPrefab);
     }
 }
